140c8aeb694f1453f2c41abb3d1b2edd9cd9cdbf
midoks <midoks@163.com>
2021-01-04 01:29:48 +0800

    Update task.py

--- task.py
+++ task.py
@@ -20,15 +20,26 @@ import db
 
 
 def printHL():
+    print('hello world')
+
+
+def videoToMp4():
+    while True:
+        print('Mp4')
+        time.sleep(2)
+
+
+def videoToM3u8():
     print 'hello world'
 
 
 def startTask():
+    import time
     # 任务队列
     try:
         time.sleep(2)
     except:
-        time.sleep(60)
+        time.sleep(5)
     startTask()
 
 # --------------------------------------PHP监控 end--------------------------------------------- #
@@ -36,6 +47,10 @@ def startTask():
 if __name__ == "__main__":
 
     t = threading.Thread(target=printHL)
+    t.setDaemon(True)
+    t.start()
+
+    t = threading.Thread(target=videoToMp4)
     t.setDaemon(True)
     t.start()
 
