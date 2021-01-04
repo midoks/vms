9ca524fc34b19dc7f98d21a6f0a5c1e514c0b989
midoks <midoks@163.com>
2021-01-05 00:21:37 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -6,6 +6,7 @@ LANG=en_US.UTF-8
 
 wget -O /tmp/main.zip https://codeload.github.com/midoks/vms/zip/main
 cd /tmp && unzip /tmp/main.zip
+rm -rf /www/wwwroot/vms
 \cp -rf  /tmp/vms_main/* /www/wwwroot/vms
 rm -rf /tmp/main.zip
 rm -rf /tmp/vms_main
