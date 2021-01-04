4792172ea74eb13eb5c9970dadc22ea3b752e334
midoks <midoks@163.com>
2021-01-05 01:17:19 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -17,13 +17,9 @@ fi
 wget -O /tmp/main.zip https://codeload.github.com/midoks/vms/zip/main
 cd /tmp && unzip /tmp/main.zip
 
+pip install -r /www/wwwroot/vms/requirements.txt
 
 
-# rm -rf /tmp/vms-main/.gitignore
-# rm -rf /tmp/vms-main/README.md
-# rm -rf /tmp/vms-main/LICENSE
 /usr/bin/cp -rf  /tmp/vms-main/* /www/wwwroot/vms
-# rm -rf /tmp/main.zip
-# rm -rf /tmp/vms-main
 
-# cd /www/wwwroot/vms && ./cli.sh restart
+cd /www/wwwroot/vms && ./cli.sh restart
