70dd8f84832364fe25b164b80a4f467e6ba6c23b
midoks <midoks@163.com>
2021-01-05 00:24:54 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -3,10 +3,11 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+unalias cp
+alias cp='cp -i'
 
 wget -O /tmp/main.zip https://codeload.github.com/midoks/vms/zip/main
 cd /tmp && unzip /tmp/main.zip
-rm -rf /www/wwwroot/vms
 \cp -rf  /tmp/vms_main/* /www/wwwroot/vms
 rm -rf /tmp/main.zip
 rm -rf /tmp/vms_main
