cdfe0ff6dd5d26ef5a9bb80469591152a706a947
midoks <midoks@163.com>
2021-01-04 16:15:39 +0800

    Update task.py

--- task.py
+++ task.py
@@ -8,6 +8,7 @@ import os
 import json
 import time
 import threading
+import subprocess
 # print sys.path
 
 sys.path.append("/usr/local/lib/python2.7/site-packages")
@@ -35,7 +36,13 @@ else:
 
 #------------Private Methods--------------
 
+def updateStatus(sid, status):
+    common.M('video_tmp').where(
+        "id=?", (sid,)).setField('status', status)
+
 # print(has_suffix, ffmpeg_cmd)
+
+
 def is_video(path):
     t = os.path.splitext(path)
     if t[1] in has_suffix:
@@ -52,10 +59,37 @@ def is_mp4(path):
 
 
 def fg_m3u8_cmd(ts_file, m3u8_file, to_file):
+    cmd = ffmpeg_cmd + ' -y -i ' + to_file + ' -c copy -map 0 -f segment -segment_list ' + \
+        m3u8_file + ' -segment_time 10 ' + ts_file
+    return cmd
+
+
+def fg_ts_cmd(ts_file, m3u8_file, to_file):
     cmd = ffmpeg_cmd + ' -y -i ' + ts_file + ' -c copy -map 0 -f segment -segment_list ' + \
         m3u8_file + ' -segment_time 10 ' + to_file
     return cmd
 #------------Private Methods--------------
+
+
+def execShell(cmdstring, cwd=None, timeout=None, shell=True):
+
+    if shell:
+        cmdstring_list = cmdstring
+    else:
+        cmdstring_list = shlex.split(cmdstring)
+    if timeout:
+        end_time = datetime.datetime.now() + datetime.timedelta(seconds=timeout)
+
+    sub = subprocess.Popen(cmdstring_list, cwd=cwd, stdin=subprocess.PIPE,
+                           shell=shell, bufsize=4096, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    while sub.poll() is None:
+        time.sleep(0.1)
+        if timeout:
+            if end_time <= datetime.datetime.now():
+                raise Exception("Timeout：%s" % cmdstring)
+
+    return sub.communicate()
 
 
 def printHL():
@@ -71,23 +105,54 @@ def videoToMp4():
             pathfile = os.getcwd() + '/tmp/' + x['filename']
             # print(pathfile)
             if is_mp4(pathfile):
-                common.M('video_tmp').where(
-                    "id=?", (x['id'],)).setField('status', 1)
+                updateStatus(x['id'], 1)
             else:
                 pass
+        time.sleep(2)
+
+
+def videoToTs():
+    while True:
+        videoM = common.M('video_tmp')
+        data = videoM.field('id,md5,filename').where('status=?', (1,)).select()
+
+        tmp_dir = os.getcwd() + '/tmp/'
+        for x in data:
+            m3u8_file = tmp_dir + x['md5'] + '/' + 'index.m3u8'
+            tofile = tmp_dir + x['md5'] + '/' + '%010d.ts'
+            pathfile = tmp_dir + x['filename']
+
+            if not os.path.exists(tmp_dir + x['md5']):
+                os.mkdir(tmp_dir + x['md5'])
+
+            cmd = fg_m3u8_cmd(tofile, m3u8_file, pathfile)
+            print(cmd)
+            print execShell(cmd)
+
         time.sleep(2)
 
 
 def videoToM3u8():
     while True:
         videoM = common.M('video_tmp')
+        data = videoM.field('id,md5,filename').where('status=?', (1,)).select()
 
-        data = videoM.field('id,filename').where('status=?', (1,)).select()
-
+        tmp_dir = os.getcwd() + '/tmp/'
+        print('videoToM3u8-----@@@start@@@-----')
         for x in data:
-            pathfile = os.getcwd() + '/tmp/' + x['filename']
-            print(pathfile)
+            m3u8_file = tmp_dir + x['md5'] + '/' + 'index.m3u8'
+            tofile = tmp_dir + x['md5'] + '/' + '%010d.ts'
+            pathfile = tmp_dir + x['filename']
 
+            if not os.path.exists(tmp_dir + x['md5']):
+                os.mkdir(tmp_dir + x['md5'])
+
+            if not os.path.exists(m3u8_file):
+                cmd = fg_m3u8_cmd(tofile, m3u8_file, pathfile)
+                data = execShell(cmd)
+                print(data[1])
+                updateStatus(x['id'], 2)
+        print('videoToM3u8-----@@@end@@@-----')
         time.sleep(2)
 
 
@@ -97,7 +162,7 @@ def startTask():
     try:
         time.sleep(2)
     except:
-        time.sleep(5)
+        time.sleep(60)
     startTask()
 
 # --------------------------------------PHP监控 end--------------------------------------------- #
@@ -111,6 +176,10 @@ if __name__ == "__main__":
     t = threading.Thread(target=videoToMp4)
     t.setDaemon(True)
     t.start()
+
+    # t = threading.Thread(target=videoToTs)
+    # t.setDaemon(True)
+    # t.start()
 
     t = threading.Thread(target=videoToM3u8)
     t.setDaemon(True)
