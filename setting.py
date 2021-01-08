2cf1e853776643e14a73a62118ef14bccdc40af6
midoks <midoks@163.com>
2021-01-08 22:59:53 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -43,7 +43,3 @@ loglevel = 'info'
 errorlog = chdir + '/logs/error.log'
 accesslog = chdir + '/logs/access.log'
 pidfile = chdir + '/logs/vms.pid'
-
-# if os.path.exists(os.getcwd() + '/data/ssl.pl'):
-#     certfile = 'ssl/certificate.pem'
-#     keyfile = 'ssl/privateKey.pem'
