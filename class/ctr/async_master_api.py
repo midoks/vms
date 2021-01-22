0264209d52075842f8d093a009350176946172f3
midoks <midoks@163.com>
2021-01-22 16:40:42 +0800

    Update async_master_api.py

--- class/ctr/async_master_api.py
+++ class/ctr/async_master_api.py
@@ -154,9 +154,10 @@ class async_master_api:
 
         vid = request.form.get('vid', '').encode('utf-8')
         mark = request.form.get('mark', '').encode('utf-8')
+        action = request.form.get('action', '1').encode('utf-8')
         # print(vid, mark)
         r = common.M('task').where(
-            'vid=? and mark=?', (vid, mark)).setField('status', 1)
+            'vid=? and mark=? and action=?', (vid, mark, action)).setField('status', 1)
 
         vlist = common.M('video_node', 'video').where(
             "pid=? and node_id=?", (vid, mark,)).select()
