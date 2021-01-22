719671cb41e53b4f02c0ff722e0b711118786127
midoks <midoks@163.com>
2021-01-22 17:19:36 +0800

    Update async_slave_api.py

--- class/ctr/async_slave_api.py
+++ class/ctr/async_slave_api.py
@@ -81,6 +81,10 @@ class async_slave_api:
         # print()
         url = nlist[0]['ip'] + ':' + nlist[0]['port']
         sign = 'from:' + url
+        if action == 2:
+            filename = request.form.get('filename', '').encode('utf-8')
+            sign = sign + ':' + filename
+
         r = common.M('task').add("ismaster,sign,vid,action,mark,status,uptime,addtime",
                                  (0, sign, vid, action, mark, 0, common.getDate(), common.getDate()))
         if not r:
